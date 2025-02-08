f64e8aaec1d58d571f4c5ce99ebd52e14e32aa39
zkElton <govada.elton@gmail.com>
2025-02-07 23:44:32 -0600

    feat: setup camera and point light

--- frontend/src/plugins/render/camera.rs
+++ frontend/src/plugins/render/camera.rs
@@ -1,34 +1,37 @@
-use bevy::{color::palettes::css::BLACK, prelude::*};
+use bevy::{asset::TrackAssets, color::palettes::css::BLACK, prelude::*};
 
 pub struct RCameraPlugin;
 impl Plugin for RCameraPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(ClearColor(Color::srgb_u8(50, 50, 50)));
         app.add_systems(Startup, setup_camera);
-        app.insert_resource(ClearColor(BLACK.into()));
     }
 }
 
 fn setup_camera(mut commands: Commands) {
-    //     // light
-    let _point_light = PointLight {
-        shadows_enabled: false,
-        ..default()
-    };
-
+    // lights
     let mut directional_light = DirectionalLight::default();
     directional_light.shadows_enabled = true;
-    directional_light.illuminance = 1000.0;
-
+    directional_light.illuminance = 2000.0;
     commands.spawn((Transform::from_xyz(4.0, 2.0, 4.0), directional_light));
-    //     // camera
 
+    // let point_light = PointLight {
+    //     shadows_enabled: false,
+    //     ..default()
+    // };
+    // let light_location = Transform::from_xyz(4.0, 4.0, -3.0);
+    // commands.spawn((
+    //     light_location.looking_at(Vec3::ZERO.with_x(4.2).with_y(4.2), Vec3::Z),
+    //     point_light,
+    // ));
+
+    // camera
     let mut projection = OrthographicProjection::default_3d();
     projection.scale = 0.008;
-
+    let camera_location = Transform::from_xyz(-5.0, -5.0, 12.0);
     commands.spawn((
         Camera3d::default(),
         Projection::Orthographic(projection),
-        Transform::from_xyz(-5.0, -5.0, 12.0)
-            .looking_at(Vec3::ZERO.with_x(4.2).with_y(4.2), Vec3::Z),
+        camera_location.looking_at(Vec3::ZERO.with_x(4.2).with_y(4.2), Vec3::Z),
     ));
 }
