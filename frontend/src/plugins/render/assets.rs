e5441faed354e0f2cc932852d2bf6d06cadb5ad7
zkElton <govada.elton@gmail.com>
2025-02-07 23:44:52 -0600

    feat: add various 3d assets

--- frontend/src/plugins/render/assets.rs
+++ frontend/src/plugins/render/assets.rs
@@ -3,26 +3,70 @@ use bevy::prelude::*;
 pub struct RAssetsPlugin;
 impl Plugin for RAssetsPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<AssetHandler>();
+        app.init_resource::<AllAssetHandles>();
         app.add_systems(Startup, load_asset);
     }
 }
 
 #[derive(Resource, Debug, Default)]
-pub struct AssetHandler {
-    pub dirt_block: Handle<Scene>,
-    pub player1: Handle<Scene>,
+pub struct AllAssetHandles {
+    pub rogue: Handle<Scene>,
+    pub mage: Handle<Scene>,
+    pub minion: Handle<Scene>,
+    pub warrior: Handle<Scene>,
+
+    pub dirt: Handle<Scene>,
+    pub floor: Handle<Scene>,
+    pub floor_detail: Handle<Scene>,
+    pub barrel: Handle<Scene>,
+    pub wall: Handle<Scene>,
+
+    pub staff: Handle<Scene>,
+    pub axe: Handle<Scene>,
+    pub blade: Handle<Scene>,
+    pub crossbow: Handle<Scene>,
+    pub shield: Handle<Scene>,
 }
 
-fn load_asset(asset_server: Res<AssetServer>, mut asset_handler: ResMut<AssetHandler>) {
-    let tile_asset_path = "kenney_minidungeon\\Models\\glb\\dirt.glb";
-    let player1_path = "kaykit_skeletons\\characters\\gltf\\Skeleton_Rogue.glb";
+fn load_asset(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // characters
+    let rogue = "kaykit_skeletons\\characters\\gltf\\Skeleton_Rogue.glb";
+    let mage = "kaykit_skeletons\\characters\\gltf\\Skeleton_Mage.glb";
+    let minion = "kaykit_skeletons\\characters\\gltf\\Skeleton_Minion.glb";
+    let warrior = "kaykit_skeletons\\characters\\gltf\\Skeleton_Warrior.glb";
 
-    let tile_handle: Handle<Scene> =
-        asset_server.load(GltfAssetLabel::Scene(0).from_asset(tile_asset_path));
-    let player1_handle: Handle<Scene> =
-        asset_server.load(GltfAssetLabel::Scene(0).from_asset(player1_path));
+    // blocks
+    let dirt = "kenney_minidungeon\\Models\\glb\\dirt.glb";
+    let floor = "kenney_minidungeon\\Models\\glb\\floor.glb";
+    let floor_detail = "kenney_minidungeon\\Models\\glb\\floor-detail.glb";
+    let barrel = "kenney_minidungeon\\Models\\glb\\barrel.glb";
+    let wall = "kenney_minidungeon\\Models\\glb\\wall.glb";
 
-    asset_handler.dirt_block = tile_handle;
-    asset_handler.player1 = player1_handle;
+    // weapons
+    let staff = "kaykit_skeletons\\assets\\gltf\\Skeleton_Staff.gltf";
+    let axe = "kaykit_skeletons\\assets\\gltf\\Skeleton_Axe.gltf";
+    let blade = "kaykit_skeletons\\assets\\gltf\\Skeleton_Blade.gltf";
+    let shield = "kaykit_skeletons\\assets\\gltf\\Skeleton_shield_Large_A.gltf";
+    let crossbow = "kaykit_skeletons\\assets\\gltf\\Skeleton_Crossbow.gltf";
+
+    let all_asset_handles = AllAssetHandles {
+        rogue: asset_server.load(GltfAssetLabel::Scene(0).from_asset(rogue)),
+        mage: asset_server.load(GltfAssetLabel::Scene(0).from_asset(mage)),
+        minion: asset_server.load(GltfAssetLabel::Scene(0).from_asset(minion)),
+        warrior: asset_server.load(GltfAssetLabel::Scene(0).from_asset(warrior)),
+
+        dirt: asset_server.load(GltfAssetLabel::Scene(0).from_asset(dirt)),
+        floor: asset_server.load(GltfAssetLabel::Scene(0).from_asset(floor)),
+        floor_detail: asset_server.load(GltfAssetLabel::Scene(0).from_asset(floor_detail)),
+        barrel: asset_server.load(GltfAssetLabel::Scene(0).from_asset(barrel)),
+        wall: asset_server.load(GltfAssetLabel::Scene(0).from_asset(wall)),
+
+        staff: asset_server.load(GltfAssetLabel::Scene(0).from_asset(staff)),
+        axe: asset_server.load(GltfAssetLabel::Scene(0).from_asset(axe)),
+        blade: asset_server.load(GltfAssetLabel::Scene(0).from_asset(blade)),
+        shield: asset_server.load(GltfAssetLabel::Scene(0).from_asset(shield)),
+        crossbow: asset_server.load(GltfAssetLabel::Scene(0).from_asset(crossbow)),
+    };
+
+    commands.insert_resource(all_asset_handles);
 }
