51452e8bc91080a934c4359fa778cd0aaa984ef8
zkElton <govada.elton@gmail.com>
2025-02-07 23:44:15 -0600

    feat: render tiles

--- frontend/src/plugins/render/tile.rs
+++ frontend/src/plugins/render/tile.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use super::{assets::AssetHandler, constants::MULTIPLIER};
+use super::assets::AllAssetHandles;
 
 pub struct RTilePlugin;
 impl Plugin for RTilePlugin {
@@ -9,13 +9,15 @@ impl Plugin for RTilePlugin {
     }
 }
 
-fn spawn_tiles(mut commands: Commands, asset_handler: Res<AssetHandler>) {
-    let grid_array = (7, 7);
+pub const YMUL: f32 = 1.2;
+pub const XMUL: f32 = 1.2;
+
+fn spawn_tiles(mut commands: Commands, asset_handler: Res<AllAssetHandles>) {
+    let grid_array = (8, 8);
     for i in 0..grid_array.0 {
         for j in 0..grid_array.1 {
-            let dirt_block = SceneRoot(asset_handler.dirt_block.clone());
-            let transform =
-                Transform::from_xyz((i as f32) * MULTIPLIER, (j as f32) * MULTIPLIER, 0.0);
+            let dirt_block = SceneRoot(asset_handler.dirt.clone());
+            let transform = Transform::from_xyz(i as f32 * XMUL, j as f32 * YMUL, 0.0);
             commands.spawn((dirt_block, transform));
         }
     }
