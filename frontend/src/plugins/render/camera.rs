75801257407d5f396f0da03d2391345865d5f72b
zkElton <govada.elton@gmail.com>
2025-02-07 18:03:38 -0600

    feat: create 3d camera plugin

--- frontend/src/plugins/render/camera.rs
+++ frontend/src/plugins/render/camera.rs
@@ -1,4 +1,7 @@
-use bevy::{color::palettes::css::BLACK, prelude::*};
+use bevy::{
+    color::palettes::css::{BLACK, BLUE, WHITE},
+    prelude::*,
+};
 
 pub struct RCameraPlugin;
 impl Plugin for RCameraPlugin {
@@ -10,16 +13,25 @@ impl Plugin for RCameraPlugin {
 
 fn setup_camera(mut commands: Commands) {
     //     // light
-    commands.spawn((
-        PointLight {
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform::from_xyz(4.0, 2.0, 4.0),
-    ));
+    let point_light = PointLight {
+        shadows_enabled: false,
+        ..default()
+    };
+
+    let mut directional_light = DirectionalLight::default();
+    directional_light.shadows_enabled = true;
+    directional_light.illuminance = 1000.0;
+
+    commands.spawn((Transform::from_xyz(4.0, 2.0, 4.0), directional_light));
     //     // camera
+
+    let mut projection = OrthographicProjection::default_3d();
+    projection.scale = 0.008;
+
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Projection::Orthographic(projection),
+        Transform::from_xyz(-5.0, -5.0, 12.0)
+            .looking_at(Vec3::ZERO.with_x(4.2).with_y(4.2), Vec3::Z),
     ));
 }
