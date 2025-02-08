120cac8145512b47ce6da1bbe95e7284e8800949
zkElton <govada.elton@gmail.com>
2025-02-08 00:26:35 -0600

    feat: modify window to borderless fullscreen

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -10,7 +10,13 @@ use plugins::{
 pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultPlugins);
+        app.add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: bevy::window::WindowMode::BorderlessFullscreen(MonitorSelection::Index(1)),
+                ..default()
+            }),
+            ..default()
+        }));
         app.add_plugins(WorldInspectorPlugin::new());
         app.add_plugins(TokioPlugin);
         app.add_plugins(ToriiPlugin);
