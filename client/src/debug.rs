63444f6a8d25016c1734c6279fc988f32814badd
Tony Stark <oktonystark8@gmail.com>
2025-02-09 18:06:11 -0600

    feat: removed old client

--- client/src/debug.rs
+++
@@ -1,6 +0,0 @@
-use bevy::app::App;
-use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-
-pub(super) fn plugin(app: &mut App) {
-    app.add_plugins((LogDiagnosticsPlugin::default(), FrameTimeDiagnosticsPlugin));
-}
--- client/src/main.rs
+++
@@ -1,7 +0,0 @@
-use bevy::prelude::*;
-
-use grid_guru::AppPlugin;
-
-fn main() {
-    App::new().add_plugins(AppPlugin).run();
-}
