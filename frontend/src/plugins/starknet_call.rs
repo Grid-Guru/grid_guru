f4a10ebaf155cc92524eb6cb623bd089ab1100d5
zkElton <govada.elton@gmail.com>
2025-02-11 00:12:44 -0600

    feat: implement key bindings to starknet txs

--- frontend/src/plugins/starknet_call.rs
+++ frontend/src/plugins/starknet_call.rs
@@ -8,7 +8,7 @@ use starknet::{
     providers::{jsonrpc::HttpTransport, JsonRpcClient, Provider, Url},
     signers::{LocalWallet, SigningKey},
 };
-use tokio::{sync::mpsc, task::AbortHandle};
+use tokio::sync::mpsc;
 
 use super::tokio::{TokioRuntimeResource, TokioRuntimeState};
 
@@ -16,6 +16,11 @@ pub struct StarknetPlugin;
 impl Plugin for StarknetPlugin {
     fn build(&self, app: &mut App) {
         app.init_state::<StarknetServerState>();
+        app.insert_resource(ClaimTilePosition {
+            x: "10",
+            y: "10",
+            current_selection: false,
+        });
         app.add_systems(
             OnEnter(TokioRuntimeState::Ready),
             spawn_starknet_caller_thread,
@@ -30,7 +35,6 @@ impl Plugin for StarknetPlugin {
 #[derive(Resource)]
 pub struct StarknetChannel {
     tx: mpsc::Sender<StarknetCommands>,
-    abort_handle: AbortHandle,
 }
 
 pub enum StarknetCommands {
@@ -38,7 +42,14 @@ pub enum StarknetCommands {
     SetAccountPlayerTwo,
     SendCreateGameTx,
     SendJoinGameTx,
-    SendClaimTileTx,
+    SendClaimTileTx(&'static str, &'static str),
+}
+
+#[derive(Resource)]
+struct ClaimTilePosition {
+    x: &'static str,
+    y: &'static str,
+    current_selection: bool,
 }
 
 #[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
@@ -48,14 +59,62 @@ enum StarknetServerState {
     Ready,
 }
 
-fn handle_player_inputs(channel: Res<StarknetChannel>, input: Res<ButtonInput<KeyCode>>) {
+fn handle_player_inputs(
+    channel: Res<StarknetChannel>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut claim_resource: ResMut<ClaimTilePosition>,
+) {
     let keys = input.get_just_pressed();
+    let mut modify_inputs = false;
+    let mut position_bool = false;
+    let mut value = "10";
+
     for key in keys {
         match key {
+            KeyCode::Digit0 => {
+                modify_inputs = true;
+                value = "0";
+            }
             KeyCode::Digit1 => {
-                let _ = channel.tx.try_send(StarknetCommands::SetAccountPlayerOne);
+                modify_inputs = true;
+                value = "1";
             }
             KeyCode::Digit2 => {
+                modify_inputs = true;
+                value = "2";
+            }
+            KeyCode::Digit3 => {
+                modify_inputs = true;
+                value = "3";
+            }
+            KeyCode::Digit4 => {
+                modify_inputs = true;
+                value = "4";
+            }
+            KeyCode::Digit5 => {
+                modify_inputs = true;
+                value = "5";
+            }
+            KeyCode::Digit6 => {
+                modify_inputs = true;
+                value = "6";
+            }
+            KeyCode::Digit7 => {
+                modify_inputs = true;
+                value = "7";
+            }
+            KeyCode::KeyX => {
+                position_bool = false;
+                claim_resource.current_selection = position_bool;
+            }
+            KeyCode::KeyY => {
+                position_bool = true;
+                claim_resource.current_selection = position_bool;
+            }
+            KeyCode::KeyQ => {
+                let _ = channel.tx.try_send(StarknetCommands::SetAccountPlayerOne);
+            }
+            KeyCode::KeyW => {
                 let _ = channel.tx.try_send(StarknetCommands::SetAccountPlayerTwo);
             }
             KeyCode::KeyN => {
@@ -64,7 +123,23 @@ fn handle_player_inputs(channel: Res<StarknetChannel>, input: Res<ButtonInput<Ke
             KeyCode::KeyJ => {
                 let _ = channel.tx.try_send(StarknetCommands::SendJoinGameTx);
             }
+            KeyCode::Space => {
+                let _ = channel.tx.try_send(StarknetCommands::SendClaimTileTx(
+                    claim_resource.x,
+                    claim_resource.y,
+                ));
+            }
             _ => {}
+        }
+
+        if modify_inputs {
+            if claim_resource.current_selection {
+                claim_resource.y = value;
+                info!("changed y selection to {value}");
+            } else {
+                claim_resource.x = value;
+                info!("changed x selection to {value}");
+            }
         }
     }
 }
@@ -76,55 +151,59 @@ fn spawn_starknet_caller_thread(
 ) {
     let (tx, mut rx) = mpsc::channel::<StarknetCommands>(64);
 
-    let abort_handle =
-        rt.0.spawn(async move {
-            let provider = get_rpc_provider().await;
-            let (signer, address) = get_player1_account();
-            let chain_id = provider.chain_id().await.unwrap();
+    let _ = rt.0.spawn(async move {
+        let provider = get_rpc_provider().await;
+        let (signer, address) = get_player1_account();
+        let chain_id = provider.chain_id().await.unwrap();
 
-            let mut account = create_player_account(provider, signer, address, chain_id);
+        let mut account = create_player_account(provider, signer, address, chain_id);
 
-            info!("Started STARKNET TX SENDING SERVER...");
-            while let Some(starknet_command) = rx.recv().await {
-                match starknet_command {
-                    StarknetCommands::SetAccountPlayerOne => {
-                        let provider = get_rpc_provider().await;
-                        let (signer, address) = get_player1_account();
-                        let chain_id = provider.chain_id().await.unwrap();
+        info!("Started STARKNET TX SENDING SERVER...");
+        while let Some(starknet_command) = rx.recv().await {
+            match starknet_command {
+                StarknetCommands::SetAccountPlayerOne => {
+                    let provider = get_rpc_provider().await;
+                    let (signer, address) = get_player1_account();
+                    let chain_id = provider.chain_id().await.unwrap();
 
-                        account = create_player_account(provider, signer, address, chain_id);
-                        info!("Switched to player one.");
-                    }
-                    StarknetCommands::SetAccountPlayerTwo => {
-                        let provider = get_rpc_provider().await;
-                        let (signer, address) = get_player2_account();
-                        let chain_id = provider.chain_id().await.unwrap();
+                    account = create_player_account(provider, signer, address, chain_id);
+                    info!("Switched to player one.");
+                }
+                StarknetCommands::SetAccountPlayerTwo => {
+                    let provider = get_rpc_provider().await;
+                    let (signer, address) = get_player2_account();
+                    let chain_id = provider.chain_id().await.unwrap();
 
-                        account = create_player_account(provider, signer, address, chain_id);
-                        info!("Switched to player two.");
+                    account = create_player_account(provider, signer, address, chain_id);
+                    info!("Switched to player two.");
+                }
+                StarknetCommands::SendCreateGameTx => {
+                    let res = send_create_game_tx(&account).await;
+                    info!("Sending a create_game transaction.");
+                    match res {
+                        Ok(tx) => info!("{tx:?}"),
+                        Err(e) => info!("{e:?}"),
                     }
-                    StarknetCommands::SendCreateGameTx => {
-                        let res = send_create_game_tx(&account).await;
-                        info!("Sending a create_game transaction.");
-                        match res {
-                            Ok(tx) => info!("{tx:?}"),
-                            Err(e) => info!("{e:?}"),
-                        }
+                }
+                StarknetCommands::SendJoinGameTx => {
+                    let res = send_join_game_tx(&account).await;
+                    match res {
+                        Ok(tx) => info!("{tx:?}"),
+                        Err(e) => info!("{e:?}"),
                     }
-                    StarknetCommands::SendJoinGameTx => {
-                        let res = send_join_game_tx(&account).await;
-                        match res {
-                            Ok(tx) => info!("{tx:?}"),
-                            Err(e) => info!("{e:?}"),
-                        }
+                }
+                StarknetCommands::SendClaimTileTx(x, y) => {
+                    let res = send_claim_tile_tx(&account, x, y).await;
+                    match res {
+                        Ok(tx) => info!("{tx:?}"),
+                        Err(e) => info!("{e:?}"),
                     }
-                    _ => {}
                 }
             }
-        })
-        .abort_handle();
+        }
+    });
 
-    commands.insert_resource(StarknetChannel { tx, abort_handle });
+    commands.insert_resource(StarknetChannel { tx });
     next_state.set(StarknetServerState::Ready);
 }
 
@@ -216,6 +295,36 @@ async fn send_join_game_tx(
             .unwrap(),
             selector: get_selector_from_name("join_game").unwrap(),
             calldata: vec![Felt::from_hex_unchecked("0x1")],
+        }])
+        .send()
+        .await;
+
+    tx
+}
+
+async fn send_claim_tile_tx(
+    account: &SingleOwnerAccount<JsonRpcClient<HttpTransport>, LocalWallet>,
+    x: &'static str,
+    y: &'static str,
+) -> Result<
+    starknet::core::types::InvokeTransactionResult,
+    starknet::accounts::AccountError<
+        starknet::accounts::single_owner::SignError<starknet::signers::local_wallet::SignError>,
+    >,
+> {
+    let calldata = vec![
+        Felt::from_hex_unchecked("0x1"),
+        Felt::from_hex_unchecked(x),
+        Felt::from_hex_unchecked(y),
+    ];
+    let tx = account
+        .execute_v3(vec![Call {
+            to: Felt::from_hex(
+                "0x0499f2e2515b64f360601510e4bf47b904ecddc50a40ce6e461e1dd4d7389398",
+            )
+            .unwrap(),
+            selector: get_selector_from_name("claim_tile").unwrap(),
+            calldata,
         }])
         .send()
         .await;
