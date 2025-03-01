bea5dac077da9bcaad89ca33452022c1a67229b6
elton-cs <govada.elton@gmail.com>
2025-02-28 21:05:50 -0700

    feat: update contracts to dojo v1.2.2

--- frontend/src/plugins/dojo_models/mod.rs
+++ frontend/src/plugins/dojo_models/mod.rs
@@ -130,28 +130,28 @@ impl From<DojoStruct> for Game {
             .ty
             .as_primitive()
             .unwrap()
-            .as_contract_address()
+            .as_class_hash()
             .unwrap()
             .into();
         let player_two = value.children[2]
             .ty
             .as_primitive()
             .unwrap()
-            .as_contract_address()
+            .as_class_hash()
             .unwrap()
             .into();
         let current_player = value.children[3]
             .ty
             .as_primitive()
             .unwrap()
-            .as_contract_address()
+            .as_class_hash()
             .unwrap()
             .into();
         let winner = value.children[4]
             .ty
             .as_primitive()
             .unwrap()
-            .as_contract_address()
+            .as_class_hash()
             .unwrap()
             .into();
         let move_count = value.children[5]
@@ -215,7 +215,7 @@ impl From<DojoStruct> for Player {
             .ty
             .as_primitive()
             .unwrap()
-            .as_contract_address()
+            .as_class_hash()
             .unwrap()
             .into();
         let score = value.children[2]
@@ -264,7 +264,7 @@ impl From<DojoStruct> for Tile {
             .ty
             .as_primitive()
             .unwrap()
-            .as_contract_address()
+            .as_class_hash()
             .unwrap()
             .into();
 
--- frontend/src/plugins/networking/config.rs
+++ frontend/src/plugins/networking/config.rs
@@ -3,9 +3,9 @@ pub const TORII_URL: &str = "http://localhost:8080";
 pub const TORII_RELAY_URL: &str = "/ip4/127.0.0.1/tcp/9090";
 
 pub const GAME_WORLD_ADDRESS: &str =
-    "0x057a3f7a51ea6dd81fc1362300aaf3cfbcd84fedf1016a5f43a0694cffea39c";
+    "0x000fb6b38cdca072996cdcf55d7f785f78bd930df1e8a5dfd4867987f254b8e7";
 pub const GAME_ACTIONS_CONTRACT_ADDRESS: &str =
-    "0x0681148eb465421c702b06cfe8d0c357ecffc884cc6bf440000d7a0afc3da3f3";
+    "0x058800d5a516d1ef714f34ec928dc1814aab40d23402148ce198981746aa41c9";
 
 pub const PLAYER_ONE_ADDRESS: &str =
     "0x127fd5f1fe78a71f8bcd1fec63e3fe2f0486b6ecd5c86a0466c3a21fa5cfcec";
